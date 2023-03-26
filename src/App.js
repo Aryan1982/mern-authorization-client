4b717288276d36ff34eebdabdab6669fd8242ab0
Aryan Thakor <62349184+Aryan1982@users.noreply.github.com>
2023-03-26 07:40:43 +0000

    asdf

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React,{useState} from "react";
 import "./App.css";
 import {BrowserRouter, Route,Routes } from 'react-router-dom';
-import Register from "./pages/register"
-import Login from './pages/login'
-import Quote from './pages/quote'
+import Register from "./pages/register";
+import Login from './pages/login';
+import Quote from './pages/quote';
+
 const App=()=> {
   return (
 
     <div className="App">
 
-          <Routes>
+        <Routes>
+
           <Route exact path="/" element={<Login/>}/>
-          <Route path="./register" element={<Register/>}/>
-          <Route path="./notes" element={<Quote/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/notes" element={<Quote/>}/>
+
         </Routes>
     </div>
 
-  );
+);
+
 }
 
 export default App;
