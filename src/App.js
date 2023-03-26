74c9c04d907bcfd7e875a4b34a59340cc8f24ce3
Aryan Thakor <62349184+Aryan1982@users.noreply.github.com>
2023-03-26 08:28:56 +0000

    fda

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{useState} from "react";
 import "./App.css";
-import {BrowserRouter, Route,Routes } from 'react-router-dom';
+import {HashRouter as Router , Route,Routes } from 'react-router-dom';
 import Register from "./pages/register";
 import Login from './pages/login';
 import Quote from './pages/quote';
@@ -9,7 +9,7 @@ const App=()=> {
   return (
 
     <div className="App">
-
+      <Router>
         <Routes>
 
           <Route exact path="/" element={<Login/>}/>
@@ -17,6 +17,7 @@ const App=()=> {
           <Route path="/notes" element={<Quote/>}/>
 
         </Routes>
+        </Router>
     </div>
 
 );
